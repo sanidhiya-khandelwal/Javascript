569ea12f648181b8cb4a408965e81d1a75acee6b
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-03-04 19:05:13 +0530

    03_conversions-operations done✅

---
+++ 01_basics/03_ConversionsOperations.js
@@ -0,0 +1,25 @@
+// conversions
+/*
+let x = 10;
+str = String(x)
+console.log(typeof str, str, typeof x, x); //string 10 number 10
+
+let y = "abc"
+num = Number(y)
+console.log(typeof y, y, typeof num, num); //string abc number NaN
+console.log(typeof y, y, typeof num, num + 1); //string abc number NaN
+*/
+
+// Operations
+
+// let x = 10;
+// let y = ++x; //prefix
+// console.log(`x : ${x} and y  : ${y}`); //x : 11 and y  : 11
+
+// let x = 10;
+// let z = x++ //postfix
+// console.log(`x : ${x} and z  : ${z}`); //x : 11 and z  : 10
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 10;
+// console.log(num1, num2, num3); //10 10 10
