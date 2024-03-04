305d7c556bc5bdf77aae689984690eb96b06329b
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-03-04 17:51:34 +0530

    done with variables✅

---
+++ 01_basics/variables.js
@@ -0,0 +1,41 @@
+
+// ***************** var ***************************
+
+/*
+var p = 25;
+{
+    p = 10;
+    console.log('p value inside block ', p); //10
+}
+console.log('p value outside block ', p); //10
+
+*/
+
+
+
+// **************** let *******************
+
+/*
+let q = 15;
+{
+    let q = 25;
+    console.log(" q value inside block ", q);
+}
+
+console.log(" q value outside block ", q);
+ */
+
+
+
+// **************** const *******************
+/*
+
+const r = 25;
+{
+    const r = 26;
+    console.log("r value inside block ", r);
+}
+console.log("r value outside block ", r);
+
+*/
+
